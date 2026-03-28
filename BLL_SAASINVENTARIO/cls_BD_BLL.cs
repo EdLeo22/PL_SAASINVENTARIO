764fc57ee8a98e4011aa578e36e0f3abdeba761c
Edisson Salgado <edileonarsalfado@gmail.com>
2026-03-27 18:56:30 -0600

    filtrar y listar

---
+++ BLL_SAASINVENTARIO/cls_BD_BLL.cs
@@ -0,0 +1,51 @@
+﻿using System.Configuration;
+using DAL_SAASINVENTARIO.BD;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BLL_SAASINVENTARIO.BD
+{
+    public class cls_BD_BLL
+    {
+        public DataTable ObtieneDTParametros()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Nombre");
+            dt.Columns.Add("Valor");
+            return dt;
+        }
+
+        public void EjecutaSP(ref cls_BD_DAL objBD)
+        {
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+
+                using (SqlConnection conn = new SqlConnection(conexion))
+                {
+                    SqlCommand cmd = new SqlCommand(objBD.sNomSP, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    foreach (DataRow row in objBD.DT_Parametros.Rows)
+                    {
+                        cmd.Parameters.AddWithValue(row["Nombre"].ToString(), row["Valor"]);
+                    }
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    objBD.DS = new DataSet();
+
+                    conn.Open();
+                    da.Fill(objBD.DS);
+                    conn.Close();
+
+                    objBD.sMsjErrorBD = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                objBD.sMsjErrorBD = ex.Message;
+            }
+        }
+    }
+}
--- PL_SAASINVENTARIO/Default.aspx.cs
+++ PL_SAASINVENTARIO/Default.aspx.cs
@@ -1,45 +1,63 @@
 ﻿using BLL_SAASINVENTARIO;
 using DAL_SAASINVENTARIO;
+using DAL_SAASINVENTARIO.BD;
 using System;
 using System.Collections.Generic;
 using System.Web.Services;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
-public partial class Default : Page
+namespace PL_SAASINVENTARIO
 {
     public partial class _Default : Page
     {
-
-    }
-
-    [WebMethod]
-    public static string CargarProductos(List<string> obj_Parametros_JS)
-    {
-        cls_Productos_DAL objDAL = new cls_Productos_DAL();
-        cls_Productos_BLL objBLL = new cls_Productos_BLL();
-
-        objDAL.iIdUsuario = Convert.ToInt32(obj_Parametros_JS[0]);
-        objDAL.sNombre = obj_Parametros_JS[1];
-        objDAL.iIdEstado = Convert.ToInt32(obj_Parametros_JS[2]);
-
-        objBLL.ListarProductos(ref objDAL);
-
-        string html = "";
-
-        if (objDAL.dtDatos != null && objDAL.dtDatos.Rows.Count > 0)
-        {
-            html += "<thead><tr>" +
-                    "<th>Nombre</th>" +
-                    "<th>Stock</th>" +
-                    "<th>Categoria</th>" +
-                    "<th>Marca</th>" +
-                    "<th>Estado</th>" +
-                    "</tr></thead><tbody>";
-
-            foreach (System.Data.DataRow row in objDAL.dtDatos.Rows)
+        protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        [WebMethod]
+        public static string CargarProductos(List<string> obj_Parametros_JS)
+        {
+            cls_Productos_DAL objDAL = new cls_Productos_DAL();
+            cls_Productos_BLL objBLL = new cls_Productos_BLL();
+
+            objDAL.iIdUsuario = Convert.ToInt32(obj_Parametros_JS[0]);
+            objDAL.sNombre = obj_Parametros_JS[1];
+            objDAL.iIdEstado = Convert.ToInt32(obj_Parametros_JS[2]);
+
+            objBLL.ListarProductos(ref objDAL);
+
+            string html = "";
+
+            if (objDAL.dtDatos != null && objDAL.dtDatos.Rows.Count > 0)
+            {
+                html += "<thead><tr>" +
+                        "<th>Nombre</th>" +
+                        "<th>Stock</th>" +
+                        "<th>Categoria</th>" +
+                        "<th>Marca</th>" +
+                        "<th>Estado</th>" +
+                        "</tr></thead><tbody>";
+
+                foreach (System.Data.DataRow row in objDAL.dtDatos.Rows)
+                {
+                    html += "<tr>" +
+                            "<td>" + row["NombreProducto"] + "</td>" +
+                            "<td>" + row["Stock"] + "</td>" +
+                            "<td>" + row["NombreCategoria"] + "</td>" +
+                            "<td>" + row["NombreMarca"] + "</td>" +
+                            "<td>" + row["Estado"] + "</td>" +
+                            "</tr>";
+                }
+
+                html += "</tbody>";
+            }
+            else
+            {
+                html = "No hay productos";
+            }
+
+            return html;
         }
     }
 }
